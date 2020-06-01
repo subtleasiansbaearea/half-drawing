668f133b1719a6ec9bb733790b86d41cc02a0113
Michael Owens <mowens@ucdavis.edu>
2020-05-31 22:53:43 -0700

    added example component HerComponent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HerComponent from './components/HerComponent';
 
 function App() {
   return (
     <div className="App">
+      <HerComponent/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
