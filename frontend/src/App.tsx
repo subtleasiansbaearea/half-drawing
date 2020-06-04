c9e59f14e4028c18eca767a994d1f71a414eb37e
Kevin <TangerineCat@users.noreply.github.com>
2020-06-04 03:32:17 -0700

    Normalize style for typescript. Delete HerComponent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
 import './App.scss';
+
+import React, { useEffect } from 'react';
+
 import DrawingComponent from './components/DrawingComponent';
 
 function App() {
@@ -7,7 +9,7 @@ function App() {
     const response = await fetch('/api/hello');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
-    
+
     return body;
   };
 
@@ -17,7 +19,7 @@ function App() {
   return (
     <div className="App">
 
-      <DrawingComponent/>
+      <DrawingComponent />
     </div>
   );
 }
