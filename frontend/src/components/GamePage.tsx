9716de764c03ac8c5c85d6ac3076a9a6ca40b134
MaveyMa <maveyma@gmail.com>
2020-06-05 20:59:51 -0700

    Created type for match prop so dynamic route works now

--- frontend/src/components/GamePage.tsx
+++ frontend/src/components/GamePage.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+ 
 //TODO make a call to database
 //check that the game id actually exists
 
-// tslint:disable-next-line 
-const GamePage = ({match, location}) => {
-  const gameID = match;
+interface Route {
+  match: {
+    params: {
+      gameID: string;
+    }
+  }
+  location: {
+
+  }
+}
+const GamePage = (match: Route) => {
+  console.log(match);
+
+  const {
+    match: {
+      params: { gameID }}
+  } = match;
+
   return (
     <>
       The game ID is {gameID}
