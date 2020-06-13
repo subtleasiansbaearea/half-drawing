696302a043fc72087d3190966d3c372a136cd47c
MaveyMa <maveyma@gmail.com>
2020-06-12 21:01:12 -0700

    I like this homepage.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,23 +8,19 @@ import './App.scss';
 import './'
 
 const App = () => {
-  // Set a full width CSS temporarily just for debugging purposes
-  const FULL_WIDTH_CSS = { width: '100%' };
 
   return (
     <div className="App">
-      <h1>Biscuits and Gravy</h1>
-      <Router>        
-        <Route exact path="/" component={HomePage}/>
-        <Route exact path="/game" component={GameNotFoundPage}/>
-        <Route exact path="/game/:gameID" component={GamePage}/>
-
-        <span style={FULL_WIDTH_CSS}>
-          <Link to="/">Home</Link><br/>
+      <span>
+        <Router>
+          {/*<Link to="/">Home</Link><br/>
           <Link to="/game">Game404</Link><br/>
-          <Link to="/game/:gameID">Game</Link>
-        </span>
-      </Router>
+          <Link to="/game/:gameID">Game</Link><br/>*/}
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/game" component={GameNotFoundPage}/>
+          <Route exact path="/game/:gameID" component={GamePage}/> 
+        </Router>
+      </span>
 
     </div>
   );
--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { useForm } from "react-hook-form";
+import { Button } from 'react-bootstrap';
+
+
 import '../styles/HomePage.scss';
 
 const HomePage = () => {
 
-	return (
-		<>
-      <div className="wrapper">
-        <h1>Biscuits and Gravy</h1>
-        <h1>OUR fancy shmancy home!</h1>
-      </div>
-
-		</>
-	);
+  return (
+  <>
+    <div className="home-wrapper">
+      <span className="name-of-game">Biscuits and Gravy</span>
+      <ScreenNameForm/>
+      <h3>
+        <span className="created-by">Kevin Tang, Michael Owens, Mavey Ma. 2020 SABA Hackathon.</span>
+      </h3>
+    </div>
+  </>
+  );
 };
+
+type ScreenName = {
+  name: string;
+  email: string;
+};
+
+const ScreenNameForm = () => {
+  const { register, handleSubmit } = useForm<ScreenName>();
+  const onSubmit = (data: ScreenName) => {
+    console.log("data", data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="inp">
+        <input
+          type="text"
+          id="name"
+          name="inp"
+          ref={register}
+          placeholder="Enter your name"
+        />
+      </div> 
+ 
+      <Button variant="success" type="submit" className="enter-button">Create Game</Button>
+    </form>
+  );
+};
+
 
 export default HomePage;
