22a87b5fcb3fc075052370c00b15e4a9164f885a
Kevin <TangerineCat@users.noreply.github.com>
2020-06-14 01:44:49 -0700

    update transport types and add websocket switching

--- src/gameService.ts
+++ src/gameService.ts
@@ -1,20 +1,50 @@
-import { Game, DrawingPair, GAME_STATE } from "../frontend/src/types/Types";
+import { ClientResponse, LOBBY_MESSAGE_TYPE, LobbyRequest } from "../frontend/src/types/Transport";
+import { DrawingPair, GAME_STATE, Game, Player } from "../frontend/src/types/Types";
 
-export const prompts: string[] = ['Biscuits and gravy', 'Mac and Cheese'];
-export const games: {[LobbyId: string]: Game} = {};
-
-export class GameImpl implements Game {
+class GameService implements Game {
   drawingPairs: { [drawingPairId: string]: DrawingPair };
-  players: {};
-  timerId: '';
+  players: { [playerId: string]: Player };
+  timerId: NodeJS.Timeout;
   state: GAME_STATE;
   hostId?: string; // playerId
 
-  constructor() {
+  constructor(timerId: NodeJS.Timeout) {
     this.drawingPairs = {};
     this.players = {};
-    this.timerId = '';
+    this.timerId = timerId;
     this.state = GAME_STATE.LOBBY;
     this.hostId = '';
   }
-};
+
+  handleLobbyMessage(message: LobbyRequest) {
+    const { lobbyType } = message;
+    switch (lobbyType) {
+      case LOBBY_MESSAGE_TYPE.NEW_PLAYER_REQUEST:
+        break;
+      case LOBBY_MESSAGE_TYPE.UPDATE_NAME_REQUEST:
+        break;
+      case LOBBY_MESSAGE_TYPE.UPDATE_READY_STATE_REQUEST:
+        break;
+      case LOBBY_MESSAGE_TYPE.START_GAME_REQUEST:
+        break;
+      case LOBBY_MESSAGE_TYPE.UPDATE_LOBBY_COMMAND:
+        break;
+    }
+  }
+
+  handleGameMessage(message: ClientResponse) {
+    const { gameState } = message;
+    switch (gameState) {
+      case GAME_STATE.LOBBY:
+        break;
+      case GAME_STATE.PHASE_ONE:
+        break;
+      case GAME_STATE.PHASE_TWO:
+        break;
+      case GAME_STATE.DISPLAY:
+        break;
+    }
+  }
+}
+
+export default GameService;
