6b87dcf84e98a49d6f94f40bbd4bed8ddc307d18
Michael Arnold Owens <xmichael.owens@gmail.com>
2020-06-14 19:31:06 -0700

    fix production serve path

--- src/server.ts
+++ src/server.ts
@@ -56,11 +56,11 @@ app.get('/lobby/all', (req: any, res: any) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
   // Handle React routing, return all requests to React app
   app.get('/', function (req: any, res: any) {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
   });
 }
 
